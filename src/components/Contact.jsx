93ed8d68d4deb4b2d6e306fe2000299d7ef54d8f
Omkesh <omkeshjadhav.dev@gmail.com>
2025-08-03 13:07:07 +0530

    Contact us page updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,13 +39,13 @@ const Contact = () => {
 
                 <div>
                     <h3>Visit Our Bar</h3>
-                    <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+                    <p>32A, Yashwin Jeevan & Orchid, Sus, Pune - 411021</p>
                 </div>
 
                 <div>
                     <h3>Contact Us</h3>
-                    <p>(555) 987-6543</p>
-                    <p>hello@jsmcocktail.com</p>
+                    <p>(+91) 987-6543-210</p>
+                    <p>helloDeveloper@gmail.com</p>
                 </div>
 
                 <div>
