0ca69abe12e7013d5d7a3355faff78a03c861f9a
Omkesh <omkeshjadhav.dev@gmail.com>
2025-08-04 22:00:55 +0530

    About section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
 
                         <div>
                             <p className="md:text-3xl text-xl font-bold">
-                                <span>4.5</span>/5
+                                <span>4.6</span>/5
                             </p>
                             <p className="text-sm text-white-100">
                                 More than +12000 customers
